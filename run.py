62a4b3480562e5694900e972771b35c288ec9310
JeffreyBull76 <jeffreybull@sky.com>
2022-10-30 02:32:26 +0000

    add early game select code for quiz

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Hi please choose a category")
+print("Python JS HTML")
+CHOS_CAT = str("1")
+
+
+def set_cat(choice):
+    if choice == "1":
+        CHOS_CAT = str("1")
+        print("category is Python")
+    elif choice == "2":
+        CHOS_CAT = str("2")
+        print("category is JS")
+    else:
+        CHOS_CAT = str("3")
+        print("category is HTML")
+
+
+def validate_choice(choice):
+    if choice not in ("1", "2", "3"):
+        print("Please choose category by typing 1, 2 or 3")
+        return False
+    else:
+        return True
+
+
+def quest_catg():
+    validated = 0
+    while validated < 1:
+        choice = input("Enter your category here 1, 2 or 3:")
+
+        if validate_choice(choice):
+            print("Thank you for validating")
+            validated += 1
+            set_cat(choice)
+
+
+quest_catg()
+print(CHOS_CAT)
