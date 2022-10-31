53986f8f5ed9dc28d8b5aa3da9a95219a6003856
JeffreyBull76 <jeffreybull@sky.com>
2022-10-31 02:08:43 +0000

    move import functions and add score updater

--- quiz.py
+++ quiz.py
@@ -1,18 +1,6 @@
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('python_quiz_questions')
-
+"""
+Onload text and define global list that holds the questions chosen
+"""
 print("Your answers are given by selecting the letter for each choice")
 print("So you if you think the answer is B you would type B\n")
 
@@ -27,7 +15,7 @@ def set_questions():
     quest_num = 1
     print("\033[1;31;40mBuilding question database...\033[0;37;48m\n")
 
-    while quest_num <= 7:
+    while quest_num <= 6:
         for i in range(1, 6):
             QLIST.extend(
                 [quest.col_values(i)[0], quest.col_values(i)[quest_num]]
@@ -83,24 +71,41 @@ def ask_question(rang1, rang2):
             quest_cnt += 1
 
 
+def game_over():
+    """
+    end game and save score function
+    """
+    print(f"Your score was {PLYR_SCORE}/6")
+    print("GAME OVER!\n")
+    name_val = 1
+
+    while name_val < 2:
+        ply_nam = input("ENTER YOUR NAME TO SAVE YOUR SCORE\n")
+
+        if ply_nam and len(ply_nam) >= 5:
+            str1 = ply_nam
+            str2 = PLYR_SCORE
+            final_str = f'{str1} ' + f'{str2}'
+            final_score = list(final_str.split(" "))
+            score_value = SHEET.worksheet("scores")
+            score_value.append_row(final_score)
+            name_val += 1
+        else:
+            print("\033[0;37;41mEnter a valid name...\033[0;37;48m")
+            print("\033[0;37;41mMinimum 5 characters !\033[0;37;48m\n")
+
+
 def play_game(val1, val2):
     """
     Allows iteration through different questions sequences
     """
     print("\033[1;36;40mSCORE = " + f'{PLYR_SCORE}\033[0;37;48m')
-    if val1 < 70:
+    if val1 < 60:
         ask_question(val1, val2)
     else:
         # sys.exit("GAME OVER!")
-        print("GAME OVER!")
-        exec(open("run.py").read())
+        game_over()     
+        # exec(open("run.py").read())
 
 
 play_game(0, 8)
-
-
-# NEXT we must track and itterate the players score
-# THEN write that score to the worksheet with a username
-
-# EXTRA WORK We must control the game over part and allow the user more control
-# over when the run.py file is executed to play again
